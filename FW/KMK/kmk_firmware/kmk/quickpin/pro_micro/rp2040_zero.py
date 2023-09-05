a13a54260f317fe6cf856eb927c9215bc5746ff6
PauloAMacias <adrianclz20@gmail.com>
2023-09-04 18:00:05 -0600

    Se elimina bases de FW QMK

---
+++ FW/KMK/kmk_firmware/kmk/quickpin/pro_micro/rp2040_zero.py
@@ -0,0 +1,27 @@
+import board
+
+pinout = [
+    None,  # RAW
+    None,  # GND
+    None,  # VCC
+    board.GP29,
+    board.GP28,
+    board.GP27,
+    board.GP26,
+    board.GP15,
+    board.GP14,
+    board.GP13,
+    board.GP12,
+    board.GP11,
+    board.GP10,
+    board.GP9,
+    board.GP8,
+    board.GP7,
+    board.GP6,
+    board.GP5,
+    board.GP4,
+    board.GP3,
+    board.GP2,
+    board.GP1,
+    board.GP0,
+]
--- FW/QMK/qmk_firmware-sepp/keyboards/splitkb/aurora/corne/keymaps/sepp/layers.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-enum layers { BASE, SYM, NUM, FUN, NAV, OTHER, MOUSE };
